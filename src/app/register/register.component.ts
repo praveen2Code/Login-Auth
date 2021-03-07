38f0d929d8a5c18328db0c839b466dcb71ca58ce
praveen.kumar <praveen.kumar@scoreme.in>
2021-03-07 11:45:33 +0530

    login-auth

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup | undefined;
+  username = '';
+  password = '';
+  name = '';
+  isLoadingResults = false;
+  matcher = new MyErrorStateMatcher();
+
+  constructor(private authService: AuthService, private router: Router, private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      username : [null, Validators.required],
+      password : [null, Validators.required],
+      name : [null, Validators.required]
+    });
+  }
+
+  onFormSubmit(): void {
+    this.isLoadingResults = true;
+    this.authService.register(this.registerForm.value)
+      .subscribe((res: any) => {
+        this.isLoadingResults = false;
+        this.router.navigate(['/login']).then(_ => console.log('You are registered now!'));
+      }, (err: any) => {
+        console.log(err);
+        this.isLoadingResults = false;
+      });
+  }
+
+}
